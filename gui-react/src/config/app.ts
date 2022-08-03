ffb80650649cc69b9bf22c99cf45160f2c423904
brianp <brian.o.pearce@gmail.com>
2022-08-03 11:10:42 +0200

    fix: define a longer container update reminder

--- gui-react/src/config/app.ts
+++ gui-react/src/config/app.ts
@@ -1,6 +1,6 @@
 export default {
   // If user dismisses downloading latest Docker image via TBot,
   // after what time we can ask user again? [ms]
-  dockerDownloadDismissValidFor: 30 * 1000, // 30 sec
+  dockerDownloadDismissValidFor: 24 * 60 * 60 * 1000, // [ms] // 24 hours
   dockerNewImagesCheckInterval: 600000, // [ms] // @TODO - change to 60000 - using larger until updated field is not fixed
 }
