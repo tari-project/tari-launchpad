38db314174cd8a3d70fd49e6171bd9fd5a387e58
brianp <brian.o.pearce@gmail.com>
2022-09-06 09:55:47 +0200

    Add the 0.38.0 tag to lock in container versions for install

--- gui-react/src/store/settings/thunks.ts
+++ gui-react/src/store/settings/thunks.ts
@@ -24,7 +24,7 @@ const getSettings = async (): Promise<InitialSettings> => {
     tariNetwork: network,
     cacheDir: newCacheDir,
     dockerRegistry: 'quay.io/tarilabs',
-    dockerTag: 'latest',
+    dockerTag: '0.38.0',
     monerodUrl: MiningConfig.defaultMoneroUrls?.join(',') || '',
     moneroUseAuth: false,
     moneroUsername: '',
