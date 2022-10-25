3133c8def842e6209f25306c229b938d66404cf9
Erika <nova@tari.com>
2022-10-25 12:23:56 +0200

    fix: typo

--- gui-react/src/locales/reset.ts
+++ gui-react/src/locales/reset.ts
@@ -3,7 +3,8 @@ export default {
     title: 'Reset Settings',
     label: 'Reset changes',
     warning: '⚠️ This action cannot be undone',
-    description: 'Clean all settings and start from scratch',
+    description:
+      'Clean all settings and start from scratch (requires restarting)',
     resetButton: 'Reset',
   },
 }
