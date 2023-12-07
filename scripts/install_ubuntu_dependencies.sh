6fa18b74d114c9b9e718abcdef8a23fc986be00a
C.Lee Taylor <47312074+leet4tari@users.noreply.github.com>
2023-12-07 19:36:27 +0200

    ci(fix): modernise - dependadbot/actions/formatting (#270)
    * ci(fix): modernise - dependadbot/actions/formatting
    
    * ci(fix): add arm64 targets for build
    
    * ci(fix): make license script happy

---
+++ scripts/install_ubuntu_dependencies.sh
@@ -0,0 +1,12 @@
+apt-get install --no-install-recommends --assume-yes \
+  build-essential \
+  libgtk-3-dev \
+  libwebkit2gtk-4.0-dev \
+  libsoup2.4-dev \
+  curl \
+  wget \
+  libappindicator3-dev \
+  patchelf \
+  librsvg2-dev \
+  libprotobuf-dev \
+  protobuf-compiler
