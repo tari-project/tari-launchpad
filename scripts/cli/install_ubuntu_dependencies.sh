4d9d60c5019b376d2d60352d51c0a01bc69be22c
C.Lee Taylor <47312074+leet4tari@users.noreply.github.com>
2024-01-12 23:46:31 +0200

    chore(ci): add init ci for tari-lp-cli (#281)

---
+++ scripts/cli/install_ubuntu_dependencies.sh
@@ -0,0 +1,23 @@
+apt-get install --no-install-recommends --assume-yes \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gpg \
+  bash \
+  less \
+  openssl \
+  libssl-dev \
+  pkg-config \
+  libsqlite3-dev \
+  libsqlite3-0 \
+  libreadline-dev \
+  git \
+  cmake \
+  dh-autoreconf \
+  libc++-dev \
+  libc++abi-dev \
+  libprotobuf-dev \
+  protobuf-compiler \
+  libncurses5-dev \
+  libncursesw5-dev \
+  zip
